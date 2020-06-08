818e9094a37241cb5c0d3e2c4daa45407a9cea4d
Stephen Houser <stephenhouser@gmail.com>
2020-06-08 17:42:29 -0400

    change to wind:

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,18 @@ void ow_read(byte data[], int length) {
   // Serial.println("");
 }
 
+bool crc8(byte data[], int length) {
+    return OneWire::crc8(data, (length - 1)) == data[(length - 1)];
+}
+
+bool crc16(byte data[], int length) {
+    uint16_t crc = OneWire::crc16(data, (length - 2));
+    uint8_t *crcBytes = (uint8_t *)&crc;
+    uint8_t crcLo = ~data[(length - 2)];
+    uint8_t crcHi = ~data[(length - 1)];
+    return !((crcLo != crcBytes[0]) || (crcHi != crcBytes[1]));
+}
+
 void printAddress(byte address[8]) {
     for (uint8_t i = 0; i < 8; i++) {
     if (address[i] < 0x10) {
@@ -56,47 +68,6 @@ void printAddress(byte address[8]) {
     Serial.print(address[i], HEX);
     Serial.print(" ");
   }  
-}
-
-int findOneWireDevices() {
-  int numberOfFoundSensors = 0;
-  byte currentAddr[8];
-  while (ow.search(currentAddr) && numberOfFoundSensors < MAX_NUMBER_OF_SENSORS) {
-    for (byte i = 0; i < 8; i++) {
-      sensor[numberOfFoundSensors].addr[i] = currentAddr[i];
-    }
-
-    String chip = "";
-    switch (currentAddr[0]) {
-        case 0x10:
-          chip = "DS18S20 Temperature";  // or old DS1820
-          break;
-        case 0x1D:
-          chip = "DS2423 RAM/Counter";
-          break;
-        case 0x20:
-          chip = "DS2450 Quad A/D";
-          break;
-        case 0x22:
-          chip = "DS1822 Temperature";
-          break;
-        case 0x28:
-          chip = "DS18B20 Temperature";
-          break;
-        case 0x29:
-          chip = "DS2408 8 Switch";
-          break;
-        default:
-          //continue;
-          chip = "Unknown";
-          break;
-    }
-        
-    sensor[numberOfFoundSensors].name = chip + "." + String(numberOfFoundSensors, DEC);
-    numberOfFoundSensors++;
-  }
-  
-  return numberOfFoundSensors;  
 }
 
 int16_t read_DSTemperature(byte address[]) {
@@ -153,18 +124,6 @@ int16_t read_DSTemperature(byte address[]) {
   // CRC of temperature reading indicates an error, so we print a error message and discard this reading
   Serial.print("ERROR: read_DSTemperature() Not present or CRC not valid");
   return 0;
-}
-
-bool crc8(byte data[], int length) {
-    return OneWire::crc8(data, (length - 1)) == data[(length - 1)];
-}
-
-bool crc16(byte data[], int length) {
-    uint16_t crc = OneWire::crc16(data, (length - 2));
-    uint8_t *crcBytes = (uint8_t *)&crc;
-    uint8_t crcLo = ~data[(length - 2)];
-    uint8_t crcHi = ~data[(length - 1)];
-    return !((crcLo != crcBytes[0]) || (crcHi != crcBytes[1]));
 }
 
 // https://github.com/jbechter/arduino-onewire-DS2423
@@ -230,6 +189,219 @@ int16_t read_DS2408(byte address[]) {
   return 0;
 }
 
+#define DS2450_WRITE_COMMAND 0x55
+#define DS2450_BEGIN_VOLTAGE_CONVERSION 0x3c
+#define DS2450_REGISTER_4CHANNEL 0x0f
+#define DS2450_READOUT_CONTROL 0xaa
+#define DS2450_MEMORY_READ_COMMAND 0xaa
+#define DS2450_8_BIT_RESOLUTION 0x08
+#define DS2450_POR_OFF_NO_ALARMS_5V_RANGE 0x01
+#define DS2450_VCC_CONTROL_BYTE_ADDR_LO 0x1c
+#define DS2450_VCC_CONTROL_BYTE_ADDR_HI 0x00
+#define DS2450_VCC_POWERED 0x40
+#define DS2450_CONTROL_STATUS_DATA_ADDR_LO 0x08
+#define DS2450_CONTROL_STATUS_DATA_ADDR_HI 0x00
+#define DS2450_CONVERSION_COMPLETE 0xff
+#define DS2450_AD_CHANNELS_ADDR_LO 0x00
+#define DS2450_AD_CHANNELS_ADDR_HI 0x00
+#define DS2450_TEMP_CONVERSION_TIMEOUT 50
+
+void setup_DS2450(byte address[]) {
+   ow.reset();
+   ow.select(address);
+   ow.write(DS2450_WRITE_COMMAND, 0);
+   ow.write(DS2450_VCC_CONTROL_BYTE_ADDR_LO, 0);
+   ow.write(DS2450_VCC_CONTROL_BYTE_ADDR_HI, 0);
+   ow.write(0x00, 0);
+   ow.read(); // crc lsb
+   ow.read(); // crc msb
+   ow.read(); // verify
+
+   ow.reset();
+   ow.select(address);
+   ow.write(DS2450_WRITE_COMMAND, 0);
+   ow.write(DS2450_CONTROL_STATUS_DATA_ADDR_LO, 0);
+   ow.write(DS2450_CONTROL_STATUS_DATA_ADDR_HI, 0);
+    for (int i = 0; i < 4; i++) {
+      ow.write(0/*DS2450_8_BIT_RESOLUTION*/, 0);
+      ow.read(); // crc
+      ow.read();
+      ow.read(); // verify data
+      ow.write(DS2450_POR_OFF_NO_ALARMS_5V_RANGE, 0);
+      ow.read(); // crc
+      ow.read();
+      ow.read(); // verify data
+    }
+  Serial.println("SETUP DS2450 Complete.");
+}
+
+#if 0
+String wind_dir(float voltages[]) {
+  float Va = voltages[0];
+  float Vb = voltages[1];
+  float Vc = voltages[2];
+  float Vd = voltages[3];
+
+  float Vmax=Va;
+  if (Vb>Vmax) Vmax=Vb;
+  if (Vc>Vmax) Vmax=Vc;
+  if (Vd>Vmax) Vmax=Vd;
+  
+  float VL1=Vmax*0.83;
+  float VL2=Vmax*0.58;
+  float VL3=Vmax*0.25;
+
+
+    a=5;
+    if (Va<VL1) a=3;
+    if (Va<VL2) a=2;
+    if (Va<VL3) a=0;
+
+    b=5;
+    if (Vb<VL1) b=3;
+    if (Vb<VL2) b=2;
+    if (Vb<VL3) b=0;
+
+    c=5;
+    if (Vc<VL1) c=3;
+    if (Vc<VL2) c=2;
+    if (Vc<VL3) c=0;
+
+    d=5;
+    if (Vd<VL1) d=3;
+    if (Vd<VL2) d=2;
+    if (Vd<VL3) d=0;
+
+
+      // N voltage=2.78, 3.95, 4.97, 4.35
+      //   voltage=2.61, 4.28, 4.59, 3.05
+      // E voltage=2.93, 4.84, 3.94, 3.48
+      //   voltage=4.56, 4.60, 4.05, 3.47
+      // S voltage=4.56, 4.88, 0.16, 3.48
+      // W voltage=0.11, 3.96, 2.74, 4.33
+
+
+    private String[] tabla={"5255", "5335","5525","5533", 
+                            "5552", "0552","0555","0055", 
+                            "5055", "5005","5505","5500",
+                            "5550", "2550","2555","3355"};
+                            
+    private String[] roseta = {"ERR"," N ", "NNW", "NW ", "WNW",
+                                     " W ", "WSW", "SW ", "SSW",
+                                     " S ", "SSE", "SE" , "ESE",
+                                     " E ", "ENE", "NE ", "NNE" };                            
+    
+
+    pos=String.valueOf(a)+String.valueOf(b)+String.valueOf(c)+String.valueOf(d);
+    for (int i=0;i<=15;i++){
+      if (pos.equals(tabla[i]))  result=i+1; 
+    }  
+}
+#endif 
+
+int16_t read_DS2450(byte address[]) {
+  if (address[0] == 0x20) {
+    // Serial.print("read_DS2450: ");
+    bool present = ow.reset();
+    if (present) {
+      byte data[13];
+
+      ow.select(address);
+      data[0] = DS2450_BEGIN_VOLTAGE_CONVERSION;    // Read PIO Registers
+      data[1] = DS2450_REGISTER_4CHANNEL;    // LSB address
+      data[2] = DS2450_READOUT_CONTROL;    // MSB address
+      ow_write(data, 3);
+
+      vTaskDelay(10 * portTICK_PERIOD_MS);     //wait for conversion ready
+
+      ow_read(data + 3, 2);
+
+      // char line[256];
+      // snprintf(line, 256, "PRE: %2.2x %2.2x", data[3], data[4]);
+      // Serial.println(line);
+
+      if (ow.reset()) {
+        ow.select(address);
+        data[0] = DS2450_MEMORY_READ_COMMAND;    // Read PIO Registers
+        data[1] = DS2450_AD_CHANNELS_ADDR_LO;    // LSB address
+        data[2] = DS2450_AD_CHANNELS_ADDR_HI;    // MSB address
+        ow_write(data, 3);
+        ow_read(data + 3, 10);
+
+        // Serial.print("READ: ");
+        // for (int i = 0; i < 13; i++) {
+        //     Serial.print(String(data[i], HEX) + " ");
+        // }
+        // Serial.println("");
+
+        if (crc16(data, 13)) {
+            float voltage[4];
+            voltage[0] = (float)((data[4] << 8) | data[3]) / 12580.0;
+            voltage[1] = (float)((data[6] << 8) | data[5]) / 12580.0;
+            voltage[2] = (float)((data[8] << 8) | data[7]) / 12580.0;
+            voltage[3] = (float)((data[10] << 8) | data[9]) / 12580.0;
+            Serial.print("voltage=");
+            Serial.print(voltage[0]);
+            Serial.print(", ");
+            Serial.print(voltage[1]);
+            Serial.print(", ");
+            Serial.print(voltage[2]);
+            Serial.print(", ");
+            Serial.print(voltage[3]);
+            Serial.println("");
+
+            return (int)voltage[0];
+        }
+      }
+    }
+  }
+
+  Serial.print("ERROR: read_DS2450() Not present or CRC not valid");
+  return 0;
+}
+
+int findOneWireDevices() {
+  int numberOfFoundSensors = 0;
+  byte currentAddr[8];
+  while (ow.search(currentAddr) && numberOfFoundSensors < MAX_NUMBER_OF_SENSORS) {
+    for (byte i = 0; i < 8; i++) {
+      sensor[numberOfFoundSensors].addr[i] = currentAddr[i];
+    }
+
+    String chip = "";
+    switch (currentAddr[0]) {
+        case 0x10:
+          chip = "DS18S20 Temperature";  // or old DS1820
+          break;
+        case 0x1D:
+          chip = "DS2423 RAM/Counter";
+          break;
+        case 0x20:
+          chip = "DS2450 Quad A/D";
+          setup_DS2450(currentAddr);
+          break;
+        case 0x22:
+          chip = "DS1822 Temperature";
+          break;
+        case 0x28:
+          chip = "DS18B20 Temperature";
+          break;
+        case 0x29:
+          chip = "DS2408 8 Switch";
+          break;
+        default:
+          //continue;
+          chip = "Unknown";
+          break;
+    }
+        
+    sensor[numberOfFoundSensors].name = chip + "." + String(numberOfFoundSensors, DEC);
+    numberOfFoundSensors++;
+  }
+  
+  return numberOfFoundSensors;  
+}
+
 void taskReadSensors(void * pvParameters) {
   numberOfSensors = findOneWireDevices();
   if (!numberOfSensors) {
@@ -252,42 +424,40 @@ void taskReadSensors(void * pvParameters) {
         case 0x1D:
           sensor[thisSensor].data.count = read_DS2423(sensor[thisSensor].addr);
           break;
+        case 0x20:  // DS2450
+          sensor[thisSensor].data.value = read_DS2450(sensor[thisSensor].addr);
+          break;
         default:
           continue;
       }    
     }
+
+    //vTaskDelay(15000 / portTICK_PERIOD_MS); // 15 seconds
   }
 }
 
 unsigned long last_ws_time = 0;
 uint32_t last_ws_count = 0;
-double rps_a[5];
-uint8_t  rps_i = 0;
-
+float last_ws_reading = 0;
 float wind_speed(uint32_t count) {
-  uint32_t elapsed_ms = millis() - last_ws_time;
-  uint32_t elapsed_count = count - last_ws_count;
+  if (last_ws_time == 0) {
+    last_ws_time = millis();
+  }
 
-  // if (elapsed_ms > 3000) {  // 5 seconds
+  uint32_t elapsed_ms = millis() - last_ws_time;
+  if (elapsed_ms > 10000) {  // 10 seconds
+    uint32_t elapsed_count = count - last_ws_count;
     last_ws_time = millis();
     last_ws_count = count;
-  // }
 
-
-  rps_a[rps_i] = ((elapsed_count * 1000.0) / elapsed_ms) / 2.0;;
-  rps_i = (rps_i + 1) % 5;
-
-  double rps = 0;
-  for (int i = 0; i < 5; i++) {
-    rps += rps_a[i];
-  }
-  rps /= 5;
-
-  if (rps >= 0.0 && ((rps * 2.453) < 200)) {
-     return (rps * 2.453) * 0.447040972; /* convert mph to m/s*/
+    // from AAGElectroinca Source for 1Wire Weather Station, TI8515.java
+    last_ws_reading = (float)((((elapsed_count * 1000.0)/(elapsed_ms)) /2.0) * 2.453) * 1862 / 3600; 
+    if (last_ws_reading > 200) {
+      last_ws_reading = 0;
+    }
   }
 
-  return 0.0;
+  return last_ws_reading;
 }
 
 void setup() {
@@ -337,10 +507,10 @@ void loop() {
             Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.count));
             {
               float ws = wind_speed(sensor[thisSensor].data.count);
-              Serial.println("\t: " + String(ws) + " m/s");
+              Serial.println("\t: " + String(ws * 0.62137120) + " mph");
             }
             break;
-          case 0x20:  // "DS2450 Quad A/D";
+          case 0x20:  // "DS2450 Quad A/D";     
           case 0x29:  // "DS2408 8 Switch";
             Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.value));
             break;
@@ -354,7 +524,7 @@ void loop() {
     Serial.println("No Dallas sensors.");
   }
   
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  vTaskDelay(15000 / portTICK_PERIOD_MS); // 15 seconds
 }
 
 
