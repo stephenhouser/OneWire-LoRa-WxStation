7847db829866cf6b0043d5c44763fbfa1cdc4b83
Stephen Houser <stephenhouser@gmail.com>
2020-06-22 12:09:36 -0400

    got wind direction working

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,42 @@
-#include <OneWire.h>
+/*
+ * OneWire Weather Station for ESP32
+ */
+
 #include <WiFi.h>
+
+// === OLED Display on ESP32
+#define OLED_ENABLED
+
+#if defined(OLED_ENABLED)
 #include <U8x8lib.h>
 
 #define DISPLAY_SDA           4
 #define DISPLAY_SDC           15
 #define DISPLAY_RESET         16
-// #define DISPLAY_ADDRESS       0x3c
 
-U8X8_SSD1306_128X64_NONAME_SW_I2C oled(DISPLAY_SDC, DISPLAY_SDA, DISPLAY_RESET);
+U8X8_SSD1306_128X64_NONAME_SW_I2C oled_display(DISPLAY_SDC, DISPLAY_SDA, DISPLAY_RESET);
+#endif
 
-//#define SHOW_DALLAS_ERROR        // uncomment to show Dallas ( CRC ) errors on Serial.
+// One Wire Bus; connections, object, and utility routines
+#include <OneWire.h>
+
 #define ONEWIRE_PIN           17   // OneWire Dallas sensors are connected to this pin
 #define MAX_NUMBER_OF_SENSORS 10   // maximum number of Dallas sensors
 #define OW_PARASITE_POWER     0     // 0 = off, 1 = on
 
-struct sensorStruct {
-  byte addr[8];
-  String name;
-  union _data {
-    float temp;
-    int count;
-    int value;
-  } data;
-} sensor[MAX_NUMBER_OF_SENSORS];
-
 OneWire  ow(ONEWIRE_PIN);        // (a 4.7K pull-up resistor is necessary)
-byte numberOfSensors;
-
 
 void ow_write(byte data[], int length) {
-  for (byte i = 0; i < length; i++) { // we need 9 bytes
+  for (byte i = 0; i < length; i++) {
     ow.write(data[i]);
   }
 }
 
 void ow_read(byte data[], int length) {
   memset(data, 0, length);
-  for (byte i = 0; i < length; i++) { // we need 9 bytes
+  for (byte i = 0; i < length; i++) {
     data[i] = ow.read();
-    // Serial.print(data[i], HEX);
-    // Serial.print(" ");
-    // if (((i + 1) % 16) == 0) {
-    //   Serial.println("");
-    // }
   }
-  // Serial.println("");
 }
 
 bool crc8(byte data[], int length) {
@@ -59,7 +51,7 @@ bool crc16(byte data[], int length) {
     return !((crcLo != crcBytes[0]) || (crcHi != crcBytes[1]));
 }
 
-void printAddress(byte address[8]) {
+void ow_print_address(byte address[8]) {
     for (uint8_t i = 0; i < 8; i++) {
     if (address[i] < 0x10) {
       Serial.print("0");
@@ -70,6 +62,24 @@ void printAddress(byte address[8]) {
   }  
 }
 
+struct sensorStruct {
+  byte addr[8];
+  String name;
+  union _data {
+    float temperature;
+    int value;
+    int count;
+    int voltage[4];
+  } data;
+} sensor[MAX_NUMBER_OF_SENSORS];
+
+byte numberOfSensors;
+
+// == Dallas Semiconductior OneWire Devices...
+
+/*
+ * DS18S20, DS18B20, DS1822 Temperature -- used for Temperature
+ */
 int16_t read_DSTemperature(byte address[]) {
   byte device_type;
   switch (address[0]) {
@@ -90,6 +100,7 @@ int16_t read_DSTemperature(byte address[]) {
 
     ow.select(address);
     ow.write(0x44, OW_PARASITE_POWER);        // start conversion, with parasite power off at the end
+
     vTaskDelay(750 / portTICK_PERIOD_MS);     //wait for conversion ready
 
     present = ow.reset();
@@ -126,32 +137,32 @@ int16_t read_DSTemperature(byte address[]) {
   return 0;
 }
 
-// https://github.com/jbechter/arduino-onewire-DS2423
-#define DS2423_READ_MEMORY_COMMAND 0xa5
-#define DS2423_PAGE_ONE 0xc0
-#define DS2423_PAGE_TWO 0xe0
-#define DS2423_COUNTER_A 0x01
-#define DS2423_COUNTER_B 0x02
-
+/*
+ * DS2423 Counter -- Used for Wind Speed
+ * 
+ * https://github.com/jbechter/arduino-onewire-DS2423
+ */
 int16_t read_DS2423(byte address[]) {
   if (address[0] == 0x1D) {
-    uint8_t data[45];
+    bool present = ow.reset();
+    if (present) {
+      uint8_t data[45];
+      data[0] = 0xa5; // DS2423_READ_MEMORY_COMMAND
+      data[1] = 0xc0; // DS2423_PAGE_ONE;
+      data[2] = 0x01;
 
-    data[0] = DS2423_READ_MEMORY_COMMAND;
-    data[1] = DS2423_PAGE_ONE;
-    data[2] = 0x01;
-    ow.reset();
-    ow.select(address);
-    ow_write(data, 3);
-    ow_read(data + 3, 42);
-    if (crc16(data, 45)) {
-      ow.reset();
-      uint32_t count = (uint32_t)data[38];
-      for (int j = 37; j >= 35; j--) {
-          count = (count << 8) + (uint32_t)data[j];
+      ow.select(address);
+      ow_write(data, 3);
+      ow_read(data + 3, 42);
+      if (crc16(data, 45)) {
+        ow.reset();
+        uint32_t count = (uint32_t)data[38];
+        for (int j = 37; j >= 35; j--) {
+            count = (count << 8) + (uint32_t)data[j];
+        }
+
+        return count;
       }
-
-      return count;
     }
   }
 
@@ -159,17 +170,19 @@ int16_t read_DS2423(byte address[]) {
   return 0;
 }
 
+/*
+ * DS2408  -- Used for Wind Direction on v1.
+ */
 int16_t read_DS2408(byte address[]) {
   if (address[0] == 0x29) {
     bool present = ow.reset();
     if (present) {
       byte data[13];
-
-      ow.select(address);
-
       data[0] = 0xF0;    // Read PIO Registers
       data[1] = 0x88;    // LSB address
       data[2] = 0x00;    // MSB address
+
+      ow.select(address);
       ow_write(data, 3);
       ow_read(data + 3, 10);
       if (crc8(data, 13)) {     // 3 cmd bytes, 6 data bytes, 2 0xFF, 2 CRC16
@@ -189,6 +202,9 @@ int16_t read_DS2408(byte address[]) {
   return 0;
 }
 
+/*
+ * DS2450 Quad A/D Converter -- Used for Wind Direction on v2 and v3.
+ */
 #define DS2450_WRITE_COMMAND 0x55
 #define DS2450_BEGIN_VOLTAGE_CONVERSION 0x3c
 #define DS2450_REGISTER_4CHANNEL 0x0f
@@ -232,58 +248,26 @@ void setup_DS2450(byte address[]) {
       ow.read();
       ow.read(); // verify data
     }
+
   Serial.println("SETUP DS2450 Complete.");
 }
 
-#if 0
-String wind_dir(float voltages[]) {
-
-  float VL1=Vmax*0.83;
-  float VL2=Vmax*0.58;
-  float VL3=Vmax*0.25;
-
-
-    private String[] tabla={"5255", "5335","5525","5533", 
-                            "5552", "0552","0555","0055", 
-                            "5055", "5005","5505","5500",
-                            "5550", "2550","2555","3355"};
-                            
-    private String[] roseta = {"ERR"," N ", "NNW", "NW ", "WNW",
-                                     " W ", "WSW", "SW ", "SSW",
-                                     " S ", "SSE", "SE" , "ESE",
-                                     " E ", "ENE", "NE ", "NNE" };                            
-    
-
-    pos=String.valueOf(a)+String.valueOf(b)+String.valueOf(c)+String.valueOf(d);
-    for (int i=0;i<=15;i++){
-      if (pos.equals(tabla[i]))  result=i+1; 
-    }  
-}
-#endif 
-
-int decode(float x, float max) {
-  if (x > (max * 0.83)) return 5;
-  if (x > (max * 0.58)) return 3;
-  if (x > (max * 0.25))  return 2;
-  return 0;
-}
-
-int16_t read_DS2450(byte address[]) {
+int16_t read_DS2450(byte address[], int voltage[]) {
   if (address[0] == 0x20) {
     // Serial.print("read_DS2450: ");
     bool present = ow.reset();
     if (present) {
       byte data[13];
-
-      ow.select(address);
       data[0] = DS2450_BEGIN_VOLTAGE_CONVERSION;    // Read PIO Registers
       data[1] = DS2450_REGISTER_4CHANNEL;    // LSB address
       data[2] = DS2450_READOUT_CONTROL;    // MSB address
+
+      ow.select(address);
       ow_write(data, 3);
+      ow_read(data + 3, 2); // CRC16 of command byte, select and control.
 
-      vTaskDelay(10 * portTICK_PERIOD_MS);     //wait for conversion ready
+      vTaskDelay(4 * portTICK_PERIOD_MS);     //wait for conversion ready
 
-      ow_read(data + 3, 2);
       if (ow.reset()) {
         ow.select(address);
         data[0] = DS2450_MEMORY_READ_COMMAND;    // Read PIO Registers
@@ -292,38 +276,15 @@ int16_t read_DS2450(byte address[]) {
         ow_write(data, 3);
         ow_read(data + 3, 10);
         if (crc16(data, 13)) {
-            float voltage[4];
-            voltage[0] = (float)((data[4] << 8) | data[3]) / 12580.0;
-            voltage[1] = (float)((data[6] << 8) | data[5]) / 12580.0;
-            voltage[2] = (float)((data[8] << 8) | data[7]) / 12580.0;
-            voltage[3] = (float)((data[10] << 8) | data[9]) / 12580.0;
-            // voltage[0] = ((data[4] << 8) | data[3]);
-            // voltage[1] = ((data[6] << 8) | data[5]);
-            // voltage[2] = ((data[8] << 8) | data[7]);
-            // voltage[3] = ((data[10] << 8) | data[9]);
-
-            Serial.print(voltage[0]);            
-            Serial.print(", ");
-            Serial.print(voltage[1]);
-            Serial.print(", ");
-            Serial.print(voltage[2]);
-            Serial.print(", ");
-            Serial.print(voltage[3]);
-            Serial.println("");
-
-            float xmax = max(voltage[0], voltage[1]);
-            xmax = max(xmax, voltage[2]);
-            xmax = max(xmax, voltage[3]);
-
-            Serial.print(decode(voltage[0], xmax));            
-            Serial.print(", ");
-            Serial.print(decode(voltage[1], xmax));
-            Serial.print(", ");
-            Serial.print(decode(voltage[2], xmax));
-            Serial.print(", ");
-            Serial.print(decode(voltage[3], xmax));
-            Serial.println("");
-
+            // float voltage[4];
+            // voltage[0] = (float)((data[4] << 8) | data[3]) / 12580.0;
+            // voltage[1] = (float)((data[6] << 8) | data[5]) / 12580.0;
+            // voltage[2] = (float)((data[8] << 8) | data[7]) / 12580.0;
+            // voltage[3] = (float)((data[10] << 8) | data[9]) / 12580.0;
+            voltage[0] = ((data[4] << 8) | data[3]);
+            voltage[1] = ((data[6] << 8) | data[5]);
+            voltage[2] = ((data[8] << 8) | data[7]);
+            voltage[3] = ((data[10] << 8) | data[9]);
             return (int)voltage[0];
         }
       }
@@ -376,39 +337,6 @@ int findOneWireDevices() {
   return numberOfFoundSensors;  
 }
 
-void taskReadSensors(void * pvParameters) {
-  numberOfSensors = findOneWireDevices();
-  if (!numberOfSensors) {
-    vTaskDelete(NULL);
-  }
-
-  while (TRUE) {
-    for (byte thisSensor = 0; thisSensor < numberOfSensors; thisSensor++) {
-      // Serial.println(sensor[thisSensor].name);
-
-      switch (sensor[thisSensor].addr[0]) { // the first ROM byte indicates which chip
-        case 0x10:  // DS18S20 or old DS1820
-        case 0x22:  // DS1822
-        case 0x28:  // DS18B20
-          sensor[thisSensor].data.temp = read_DSTemperature(sensor[thisSensor].addr);
-          break;
-        case 0x29:  // DS2408
-          sensor[thisSensor].data.value = read_DS2408(sensor[thisSensor].addr);
-          break;
-        case 0x1D:
-          sensor[thisSensor].data.count = read_DS2423(sensor[thisSensor].addr);
-          break;
-        case 0x20:  // DS2450
-          sensor[thisSensor].data.value = read_DS2450(sensor[thisSensor].addr);
-          break;
-        default:
-          continue;
-      }    
-    }
-
-    //vTaskDelay(15000 / portTICK_PERIOD_MS); // 15 seconds
-  }
-}
 
 unsigned long last_ws_time = 0;
 uint32_t last_ws_count = 0;
@@ -434,14 +362,117 @@ float wind_speed(uint32_t count) {
   return last_ws_reading;
 }
 
+/*
+ * Decode Wind Direction from quad voltages reported by DS2450
+ */
+int wind_direction_decode(int voltage) {
+   if (voltage > 47000) return 2;
+   if (voltage > 15000) return 1;
+   return 0;
+}
+
+String wind_direction_rose(int voltage[4]) {
+  // Wind direction can be considered a base 3 number with four digits
+  int direction = wind_direction_decode(voltage[0]) * 27
+    + wind_direction_decode(voltage[1]) * 9
+    + wind_direction_decode(voltage[2]) * 3
+    + wind_direction_decode(voltage[3]);
+
+  switch (direction) {
+    case 8:  return "ENE";  // 292.5;
+    case 25: return "ESE";  // 247.5;
+    case 26: return "E";    // 270.0;
+    case 44: return "WSW";  // 112.5;
+    case 51: return "WNW";  //  67.5;
+    case 53: return "W";    //  90.0;
+    case 56: return "NNE";  // 337.5;
+    case 62: return "NE";   // 315.0;
+    case 68: return "SSW";  // 157.5;
+    case 71: return "SW";   // 135.0;
+    case 72: return "NNW";  //  22.5;
+    case 74: return "N";    //   0.0;
+    case 76: return "SSE";  // 202.5;
+    case 77: return "S";    // 180.0;
+    case 78: return "NW";  //  45.0;
+    case 79: return "SE";  // 225.0;
+  }
+
+  Serial.println("DIR=" + String(direction));
+  return "Err";
+}
+
+float wind_direction(int voltage[4]) {
+  // Wind direction can be considered a base 3 number with four digits
+  int direction = wind_direction_decode(voltage[0]) * 27
+    + wind_direction_decode(voltage[1]) * 9
+    + wind_direction_decode(voltage[2]) * 3
+    + wind_direction_decode(voltage[3]);
+
+  switch (direction) {
+    case 8:  return 292.5; // ENE
+    case 25: return 247.5; // ESE
+    case 26: return 270.0; // E
+    case 44: return 112.5; // WSW
+    case 51: return  67.5; // WNW
+    case 53: return  90.0; // W
+    case 56: return 337.5; // NNE
+    case 62: return 315.0; // NE
+    case 68: return 157.5; // SSW
+    case 71: return 135.0; // SW
+    case 72: return  22.5; // NNW
+    case 74: return   0.0; // N
+    case 76: return 202.5; // SSE
+    case 77: return 180.0; // S
+    case 78: return  45.0; // NW
+    case 79: return 225.0; // SE
+  }
+
+  Serial.println("DIR=" + String(direction));
+  return -1.0;
+}
+
+void taskReadSensors(void * pvParameters) {
+  numberOfSensors = findOneWireDevices();
+  if (!numberOfSensors) {
+    vTaskDelete(NULL);
+  }
+
+  while (TRUE) {
+    for (byte thisSensor = 0; thisSensor < numberOfSensors; thisSensor++) {
+      switch (sensor[thisSensor].addr[0]) { // the first ROM byte indicates which chip
+        case 0x10:  // DS18S20 or old DS1820
+        case 0x22:  // DS1822
+        case 0x28:  // DS18B20
+          sensor[thisSensor].data.temperature = read_DSTemperature(sensor[thisSensor].addr);
+          break;
+        case 0x29:  // DS2408
+          sensor[thisSensor].data.value = read_DS2408(sensor[thisSensor].addr);
+          break;
+        case 0x1D:
+          sensor[thisSensor].data.count = read_DS2423(sensor[thisSensor].addr);
+          break;
+        case 0x20:  // DS2450
+          read_DS2450(sensor[thisSensor].addr, sensor[thisSensor].data.voltage);
+          break;
+        default:
+          continue;
+      }    
+    }
+
+    vTaskDelay(10000 / portTICK_PERIOD_MS); // 15 seconds
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting...");
 
-  oled.begin();
-  oled.setFont(u8x8_font_8x13_1x2_f);
-  oled.setCursor(0, 0);
-  oled.print("Starting...\n!");
+  #if defined(OLED_ENABLED)
+  oled_display.begin();
+  oled_display.setFont(u8x8_font_8x13_1x2_f);
+  oled_display.setCursor(0, 0);
+  oled_display.print("Starting...\n");
+  #endif
 
   xTaskCreatePinnedToCore(
     taskReadSensors,                /* Function to implement the task */
@@ -460,41 +491,61 @@ void setup() {
   // }
 }
 
+//
+//
+//
+//
+
 void loop() {
   if (numberOfSensors) {
-    // display.clear();
-#if 0
+    #if defined(OLED_ENABLED)
+    oled_display.clear();
+    #endif
+
     Serial.print("\n\n");
     Serial.print(String(millis() / 1000.0) + " sec");
     Serial.printf(" %i Dallas sensors found.\n", numberOfSensors);
     
     for (byte thisSensor = 0; thisSensor < numberOfSensors; thisSensor++) {
-      printAddress(sensor[thisSensor].addr);
+      ow_print_address(sensor[thisSensor].addr);
 
       switch (sensor[thisSensor].addr[0]) {
           case 0x10:
           case 0x22:
           case 0x28:
-            Serial.println(sensor[thisSensor].name + ": " + String((float)sensor[thisSensor].data.temp / 16.0) + "°C");
+            Serial.println(sensor[thisSensor].name + ": " + String((float)sensor[thisSensor].data.temperature / 16.0) + "°C");
             break;
           case 0x1D:  // "DS2423 RAM/Counter";
             Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.count));
             {
               float ws = wind_speed(sensor[thisSensor].data.count);
-              Serial.println("\t: " + String(ws * 0.62137120) + " mph");
+              String ws_line = String(ws * 0.62137120) + " mph";
+              Serial.println(ws_line);
+              #if defined(OLED_ENABLED)
+              oled_display.drawString(5, 6, ws_line.c_str());
+              #endif
             }
             break;
           case 0x20:  // "DS2450 Quad A/D";     
+            Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.value));
+            {
+              String wd = wind_direction_rose(sensor[thisSensor].data.voltage);
+              Serial.println("\t: " + wd);
+              #if defined(OLED_ENABLED)
+              oled_display.drawString(0, 7, wd.c_str());
+              #endif
+            }
+            break;
+
           case 0x29:  // "DS2408 8 Switch";
             Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.value));
             break;
+
           default:
-            //continue;
             Serial.println(sensor[thisSensor].name + ": " + String(sensor[thisSensor].data.value));
             break;
       }
     }
-  #endif
   } else {
     Serial.println("No Dallas sensors.");
   }
